52c64dc7c4d30675e61b2d08e6acfe2ee71d2465
Jonathan Kolman <jonathankolman@Jonathans-MBP.fios-router.home>
2021-03-15 14:04:22 -0400

    initial

---
+++ app.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox(executable_path='/Users/jonathankolman/Downloads/Geckodriver')
+
+
+browser.get('https://google.com')
+
+
+
+
